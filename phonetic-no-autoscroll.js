8b1b303d6191cfb880ec7b9ed429c05e28e842b9
Vardath <59270526+Vardath@users.noreply.github.com>
2026-09-10 14:09:05 +1000

    Load completed original-language dictionary UI

--- phonetic-no-autoscroll.js
+++ phonetic-no-autoscroll.js
@@ -66,7 +66,7 @@
   hp.defer=true; document.head.appendChild(hp);
 
   const od=document.createElement('script');
-  od.src='phonetic-original-language-dictionary.js?v=20260910-dict1';
+  od.src='phonetic-original-language-dictionary.js?v=20260910-full1';
   od.defer=true; document.head.appendChild(od);
 
   if(location.hash){ try{ history.replaceState(null,'',location.pathname+location.search); }catch(_){ } }
