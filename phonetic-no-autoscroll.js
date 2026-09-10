5f3dfada1271399f51bc439a3282db2fe265abc3
Vardath <59270526+Vardath@users.noreply.github.com>
2026-09-10 14:25:54 +1000

    Refresh visible dictionary sort controls

--- phonetic-no-autoscroll.js
+++ phonetic-no-autoscroll.js
@@ -66,7 +66,7 @@
   hp.defer=true; document.head.appendChild(hp);
 
   const od=document.createElement('script');
-  od.src='phonetic-original-language-dictionary.js?v=20260910-grid6';
+  od.src='phonetic-original-language-dictionary.js?v=20260910-sort7';
   od.defer=true; document.head.appendChild(od);
 
   if(location.hash){ try{ history.replaceState(null,'',location.pathname+location.search); }catch(_){ } }
